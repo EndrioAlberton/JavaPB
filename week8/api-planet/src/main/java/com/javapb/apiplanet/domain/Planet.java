85298a0f82097b76b182ef279bc8b8571beb8653
EndrioAlberton <endrio.alberton@gmail.com>
2024-01-31 15:21:26 -0300

    added: session 3(integration tests)

--- week8/api-planet/src/main/java/com/javapb/apiplanet/domain/Planet.java
+++ week8/api-planet/src/main/java/com/javapb/apiplanet/domain/Planet.java
@@ -1,46 +1,94 @@
 package com.javapb.apiplanet.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "planets")
 public class Planet {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	private String name;
-	private String climate;
-	private String terrain;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getClimate() {
-		return climate;
-	}
-	public void setClimate(String climate) {
-		this.climate = climate;
-	}
-	public String getTerrain() {
-		return terrain;
-	}
-	public void setTerrain(String terrain) {
-		this.terrain = terrain;
-	}
-	
-	
+  @NotEmpty
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @NotEmpty
+  @Column(nullable = false)
+  private String climate;
+
+  @NotEmpty
+  @Column(nullable = false)
+  private String terrain;
+
+  public Planet() {
+  }
+
+  public Planet(String climate, String terrain) {
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Planet(String name, String climate, String terrain) {
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Planet(Long id, String name, String climate, String terrain) {
+    this.id = id;
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getClimate() {
+    return climate;
+  }
+
+  public void setClimate(String climate) {
+    this.climate = climate;
+  }
+
+  public String getTerrain() {
+    return terrain;
+  }
+
+  public void setTerrain(String terrain) {
+    this.terrain = terrain;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(obj, this);
+  }
+
+  @Override
+  public String toString() {
+    return "Planet [climate=" + climate + ", id=" + id + ", name=" + name + ", terrain=" + terrain + "]";
+  }
 }
